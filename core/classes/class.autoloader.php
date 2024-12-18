67bc7d0ecc690e106b1fab54c33d58633f89f7cb
Bear <programming@hallhome.us>
2024-12-17 20:59:05 -0600

    local host now loads

--- core/classes/class.autoloader.php
+++ core/classes/class.autoloader.php
@@ -7,17 +7,17 @@
  * Github: https://github.com/Bearsampp
  */
 
+declare(strict_types=1);
+
 /**
  * Class Autoloader
  *
- * This class handles the autoloading of classes within the Bearsampp application.
+ * Handles the autoloading of classes within the Bearsampp application.
  * It registers itself with the SPL autoload stack and loads classes based on naming conventions.
  */
 class Autoloader
 {
     /**
-     * Autoloader constructor.
-     *
      * Initializes the Autoloader object.
      */
     public function __construct()
@@ -30,32 +30,34 @@ class Autoloader
      * @param string $class The name of the class to load.
      * @return bool True if the class file was successfully loaded, false otherwise.
      */
-    public function load($class)
+    public function load(string $class): bool
     {
         global $bearsamppRoot;
 
-        $class = strtolower($class);
+        $class    = strtolower($class);
         $rootPath = $bearsamppRoot->getCorePath();
 
         $file = $rootPath . '/classes/class.' . $class . '.php';
+
         if (Util::startWith($class, 'bin')) {
-            $class = $class != 'bins' ? substr_replace($class, '.', 3, 0) : $class;
-            $file = $rootPath . '/classes/bins/class.' . $class . '.php';
+            $classFileName = ($class !== 'bins') ? substr_replace($class, '.', 3, 0) : $class;
+            $file          = $rootPath . '/classes/bins/class.' . $classFileName . '.php';
         } elseif (Util::startWith($class, 'tool')) {
-            $class = $class != 'tools' ? substr_replace($class, '.', 4, 0) : $class;
-            $file = $rootPath . '/classes/tools/class.' . $class . '.php';
+            $classFileName = ($class !== 'tools') ? substr_replace($class, '.', 4, 0) : $class;
+            $file          = $rootPath . '/classes/tools/class.' . $classFileName . '.php';
         } elseif (Util::startWith($class, 'app')) {
-            $class = $class != 'apps' ? substr_replace($class, '.', 3, 0) : $class;
-            $file = $rootPath . '/classes/apps/class.' . $class . '.php';
+            $classFileName = ($class !== 'apps') ? substr_replace($class, '.', 3, 0) : $class;
+            $file          = $rootPath . '/classes/apps/class.' . $classFileName . '.php';
         } elseif (Util::startWith($class, 'action')) {
-            $class = $class != 'action' ? substr_replace($class, '.', 6, 0) : $class;
-            $file = $rootPath . '/classes/actions/class.' . $class . '.php';
-        } elseif (Util::startWith($class, 'tplapp') && $class != 'tplapp') {
-            $class = substr_replace(substr_replace($class, '.', 3, 0), '.', 7, 0);
-            $file = $rootPath . '/classes/tpls/app/class.' . $class . '.php';
+            $classFileName = ($class !== 'action') ? substr_replace($class, '.', 6, 0) : $class;
+            $file          = $rootPath . '/classes/actions/class.' . $classFileName . '.php';
+        } elseif (Util::startWith($class, 'tplapp') && $class !== 'tplapp') {
+            $classFileName = substr_replace($class, '.', 3, 0);
+            $classFileName = substr_replace($classFileName, '.', 7, 0);
+            $file          = $rootPath . '/classes/tpls/app/class.' . $classFileName . '.php';
         } elseif (Util::startWith($class, 'tpl')) {
-            $class = $class != 'tpls' ? substr_replace($class, '.', 3, 0) : $class;
-            $file = $rootPath . '/classes/tpls/class.' . $class . '.php';
+            $classFileName = ($class !== 'tpls') ? substr_replace($class, '.', 3, 0) : $class;
+            $file          = $rootPath . '/classes/tpls/class.' . $classFileName . '.php';
         }
 
         if (!file_exists($file)) {
@@ -71,9 +73,9 @@ class Autoloader
      *
      * @return bool True on success, false on failure.
      */
-    public function register()
+    public function register(): bool
     {
-        return spl_autoload_register(array($this, 'load'));
+        return spl_autoload_register([$this, 'load']);
     }
 
     /**
@@ -81,8 +83,8 @@ class Autoloader
      *
      * @return bool True on success, false on failure.
      */
-    public function unregister()
+    public function unregister(): bool
     {
-        return spl_autoload_unregister(array($this, 'load'));
+        return spl_autoload_unregister([$this, 'load']);
     }
 }
